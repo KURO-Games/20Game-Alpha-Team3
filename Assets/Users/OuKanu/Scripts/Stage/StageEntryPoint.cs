97de54af3f927d940735334e1b6e97feecddbd69
Wang Gaunyu <smartwgy@gmail.com>
2020-06-23 18:51:40 +0900

    [New]ステージ管理用のスクリプトを追加

---
+++ Assets/Users/OuKanu/Scripts/Stage/StageEntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageEntryPoint : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetEntryPoint;
+
+
+    public Vector3 GetTargetStageEntryPostion()
+    {
+        return targetEntryPoint.position;
+    }
+
+    
+}
---
+++ Assets/Users/OuKanu/Scripts/Stage/StageManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> StagePrefabs = new List<GameObject>();
+
+    public List<GameObject> Stages;
+
+    private void Awake()
+    {
+        Stages = new List<GameObject>();
+    }
+
+
+    public void CreateStage(GameObject stageOrigin)
+    {
+        var stage = Instantiate<GameObject>(stageOrigin);
+        Stages.Add(stage);
+        stage.SetActive(false);
+    }
+
+    public void LoadStage(GameObject stage,Vector3 position)
+    {
+        stage.transform.position = position;
+        stage.SetActive(true);
+    }
+
+    public void UnloadStage(GameObject stage)
+    {
+        stage.SetActive(false);
+    }
+
+    public void DeleteStage(GameObject stage)
+    {
+        Stages.Remove(stage);
+        Destroy(stage);
+    }
+
+}
